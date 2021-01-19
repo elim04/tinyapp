af7a017dd277ccbe48b9afb0fb648709d88bbc6b
Emily Lim <emily.elim04@gmail.com>
2021-01-19 20:12:37 +0000

    implementing displaying username on header of each page when logged in

--- express_server.js
+++ express_server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port is 8080
 const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
 
 //Function for generating random string of 6 characters
 const generateRandomString = function() {
@@ -20,6 +19,8 @@ const generateRandomString = function() {
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,12 +29,18 @@ const urlDatabase = {
 
 //Routing
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase}; //note when sending variables to EJS template, we need to send them inside an object
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  }; //note when sending variables to EJS template, we need to send them inside an object
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars ={
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -42,7 +49,11 @@ app.get("/urls/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   if (longURL) {
     
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = { 
+      username: req.cookies["username"],
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL]
+     };
     res.render("urls_shows", templateVars);
 
   } else {
@@ -62,6 +73,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 //Posting
+
 app.post("/urls", (req, res) => {
   console.log(req.body);
   let id = generateRandomString();
@@ -80,7 +92,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //to Update a long url 
 app.post("/urls/:shortURL", (req, res) => {
   let newURL = req.body.newURL;
-  urlDatabase[req.params.shortURL] = req.body.newURL;
+  urlDatabase[req.params.shortURL] = newURL;
+  res.redirect("/urls");
+});
+
+//to login to your profile route
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
   res.redirect("/urls");
 });
 
