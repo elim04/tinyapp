cbd0b35a7f3754eee6f30315c52f07f2ecf5fcda
Emily Lim <emily.elim04@gmail.com>
2021-01-19 01:02:14 +0000

    creating redirects after form submission

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080; //default port is 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Generate random shortURL function
+//Function for generating random string of 6 characters
 function generateRandomString() {
   const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let result = "";
@@ -17,11 +17,6 @@ function generateRandomString() {
 
   return result;
 }
-
-console.log(generateRandomString());
-
-
-
 
 app.set("view engine", "ejs");
 
@@ -43,14 +38,33 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   //route parameter is req.params.shortURL
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
-  res.render("urls_shows", templateVars);
+
+  if (req.params.shortURL) {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
+    res.render("urls_shows", templateVars);
+  } else {
+    res.status(404).render('404');
+  }
+
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
+
 
 //Posting
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("Ok");
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
+  
 });
 
 app.listen(PORT, () => {
