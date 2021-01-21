b611004fc3243c3b1f9cb786dc820bdf35a337bd
Emily Lim <emily.elim04@gmail.com>
2021-01-21 19:24:35 +0000

    created helpers.js to store all helper functions and import into express server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const {generateRandomString, getUserByEmail, userAlreadyExists, userAuthenticator, userIDReturner, urlsForUser} = require('./helpers');
 const bodyParser = require("body-parser");
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 // const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
@@ -14,64 +15,6 @@ app.use(cookieSession({
   keys: ['key1', 'key2']
 }));
 // app.use(cookieParser());
-
-//Function for generating random string of 6 characters
-const generateRandomString = function() {
-  const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  let result = "";
-
-  for (let i = 0; i < 6; i++) {
-    let index = Math.ceil(Math.random() * 61);
-    result += characters[index];
-  }
-
-  return result;
-};
-
-//helper function to check if user exists in database
-const userAlreadyExists = function(userDatabase, email) {
-  for (let user in userDatabase) {
-    if (userDatabase[user].email === email) {
-      return true;
-    } 
-  }
-};
-
-//helper function to check for correct password for user email first check if email exists then password
-
-const userAuthenticator = function(userDatabase, email, password) {
-  for (let user in userDatabase) {
-    if (userDatabase[user].email === email) {
-      if (bcrypt.compareSync(password, userDatabase[user].password)) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-//helper function to return userID
-const userIDReturner = function(userDatabase, email) {
-  for (let user in userDatabase) {
-    if (userDatabase[user].email === email) {
-      let userID = userDatabase[user].id;
-      return userID;
-    } 
-  }
-};
-
-//function for returning uRLS when userID is equal to id of currently logged in user
-const urlsForUser = function(urlDatabase, userID) {
-  let userSpecificURLS = {};
-
-  for (let key in urlDatabase) {
-    if (userID === urlDatabase[key].userID) {
-      userSpecificURLS[key] = urlDatabase[key];
-    }
-  }
-  // console.log("userSpecificURLS", userSpecificURLS)
-  return userSpecificURLS;
-};
 
 app.set("view engine", "ejs");
 
@@ -199,7 +142,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
       res.redirect("/urls");
     } 
   } else {
-    //status code FORBIDDION ACTION
+    //status code Forbiddon ooooou
     res.sendStatus(403);
   }
 
---
+++ helpers.js
@@ -0,0 +1,73 @@
+//helpers.js 
+//helper functions for express_server.js
+const bcrypt = require('bcrypt');
+
+//Function for generating random string of 6 characters
+const generateRandomString = function() {
+  const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let result = "";
+
+  for (let i = 0; i < 6; i++) {
+    let index = Math.ceil(Math.random() * 61);
+    result += characters[index];
+  }
+
+  return result;
+};
+
+//helper function to retrieve user
+
+const getUserByEmail = function(email, userDatabase) {
+  for (let user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      return user;
+    }
+  }
+};
+
+//helper function to check if user exists in database
+const userAlreadyExists = function(userDatabase, email) {
+  for (let user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      return true;
+    } 
+  }
+};
+
+//helper function to check for correct password for user email first check if email exists then password
+
+const userAuthenticator = function(userDatabase, email, password) {
+  for (let user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      if (bcrypt.compareSync(password, userDatabase[user].password)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+//helper function to return userID
+const userIDReturner = function(userDatabase, email) {
+  for (let user in userDatabase) {
+    if (userDatabase[user].email === email) {
+      let userID = userDatabase[user].id;
+      return userID;
+    } 
+  }
+};
+
+//function for returning uRLS when userID is equal to id of currently logged in user
+const urlsForUser = function(urlDatabase, userID) {
+  let userSpecificURLS = {};
+
+  for (let key in urlDatabase) {
+    if (userID === urlDatabase[key].userID) {
+      userSpecificURLS[key] = urlDatabase[key];
+    }
+  }
+  // console.log("userSpecificURLS", userSpecificURLS)
+  return userSpecificURLS;
+};
+
+module.exports = {generateRandomString, getUserByEmail, userAlreadyExists, userAuthenticator, userIDReturner, urlsForUser};
